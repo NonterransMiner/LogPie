943a3c8c9686b505a96abf426e60bd4b6606c797
Eizo <eizoassik@gmail.com>
2014-08-23 02:49:36 +0800

    fix importing problem 1) fixed importing problems by adding LogPie's root path to sys.path 2) adjusted generating, no more copy of generic.py, now use import, and 3) use a template string instand of making a list of sentences 4) now generate & its generated files could be used im command-line

--- reader/generate.py
+++ reader/generate.py
@@ -2,11 +2,21 @@
 """
 This module makes the reader.
 """
-import sys, os, importlib, time, math, shutil
-
-import pattern
+import sys
+import os
+import importlib
+import time
+import shutil
 
 READER_TPL = '''
+import sys
+
+sys.path.append(\'{pie_root}\')
+
+{directive_import}
+
+from reader.generic import GeneralReader
+
 class {s}LogReader(GeneralReader):
     def __init__(self, lines):
         super().__init__(lines)
@@ -18,6 +28,8 @@ def main():
     if len(sys.argv) == 2:
         reader = {s}LogReader(sys.argv[1])
         reader.readall()
+    else:
+        print("Usage:", '\\n', '$ python {fn} logfile')
 
 if __name__ == '__main__':
     main()
@@ -25,9 +37,12 @@ if __name__ == '__main__':
 
 
 def gen_filepath(s: str, root: str):
-    return root + '/{}_{}.py'.format(s,
-                                     # math.floor(time.time()) % 10000)
-                                     0000)
+    suffix = time.time() % 10000
+    new_path = root + '/%s_%04d.py' % (s, suffix)
+    while os.path.exists(new_path):
+        suffix += 1
+        new_path = root + '/%s_%04d.py' % (s, suffix)
+    return new_path
 
 
 def triad_to_string(triad, indent: int=3):
@@ -38,34 +53,34 @@ def triad_to_string(triad, indent: int=3):
     return ts
 
 
-def write_code(s: str, src_path: str, regexp: str, triads: list):
+def write_code(src_path: str, pie_root: str, s: str, regexp: str, triads: list):
     class_names = map(lambda triad: triad[1].__name__, triads)
+    src_filename = os.path.split(src_path)[1]
     readable_triad = "[\n{}]".format(', \n'.join(map(triad_to_string, triads)))
     # add imports
-    src = ['\n', 'import sys\n']
+    imports = []
     for name in class_names:
-        src.append('from reader.pattern.{} import {}\n'.format(s, name))
-    src.append('\n')
+        imports.append('from reader.pattern.{} import {}'.format(s, name))
     # format the class
-    src.append(READER_TPL
-               .format(s=s.title(),
-                       regexp=repr(regexp),
-                       triads=readable_triad))
-    # print('\n'.join(src))
+    src = READER_TPL.format(s=s.title(), fn=src_filename, pie_root=pie_root,
+                            regexp=repr(regexp), triads=readable_triad,
+                            directive_import='\n'.join(imports))
     with open(src_path, 'a') as src_file:
         src_file.writelines(src)
 
 
 def make_reader(s, pattern):
     # get absolute root of package reader
-    this_file_abs = os.path.abspath(sys.argv[0])
-    abs_root = os.path.split(this_file_abs)[0]
-    abs_gen = abs_root + '/gen'
+    generate_abp = os.path.abspath(sys.argv[0])
+    reader_root = os.path.split(generate_abp)[0]
+    gen_root = reader_root + '/gen'
+    pie_root = os.path.split(reader_root)[0]
     # init gen as a empty module
-    if not os.path.exists(abs_gen):
-        os.mkdir(abs_gen)
-        shutil.copy(abs_root + '/__init__.py', abs_gen + '/__init__.py')
+    if not os.path.exists(gen_root):
+        os.mkdir(gen_root)
+        shutil.copy(reader_root + '/__init__.py', gen_root + '/__init__.py')
     # dynamic import the given pattern module
+    importlib.import_module('pattern')
     pm = importlib.import_module('.' + s.lower(), 'pattern')
     # print(pm, hasattr(pm, 'parser'), dir(pm))
     if not hasattr(pm, 'parser'):
@@ -75,14 +90,20 @@ def make_reader(s, pattern):
         pattern_parser = pm.parser
         regexp, build_triads = pattern_parser(pattern)
         # initialize the new reader's source file
-        filepath = gen_filepath(s, abs_gen)
-        if os.path.exists(filepath):
-            os.remove(filepath)
-        shutil.copy(abs_root + '/generic.py', filepath)
+        filepath = gen_filepath(s, gen_root)
         # at last then write in generated new code
-        write_code(s, filepath, regexp, build_triads)
+        write_code(filepath, pie_root, s, regexp, build_triads)
         # print(regexp, '\n', build_triads)
 
 
+def main():
+    if len(sys.argv) == 3:
+        make_reader(*sys.argv[1:])
+    else:
+        print('Usage:', '\t$ python generate.py type pattern',
+              'Example:', '\t$ python generate.py systemd "$d %h %s: %m"',
+              sep='\n')
+
+
 if __name__ == '__main__':
-    make_reader('systemd', '%d %h %s: %m')
+    main()
