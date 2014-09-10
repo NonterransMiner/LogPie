29d083690e865f81b2dbdc4d8d11001ff15bc5b2
Eizo <eizoassik@gmail.com>
2014-09-10 13:30:04 +0800

    init

---
+++ logpie.py
@@ -0,0 +1,52 @@
+# encoding=utf-8
+"""
+The main interface of LogPie
+"""
+
+import json
+import logging
+from reader.generate import make_reader
+
+CONFIGURATION = 'logpie.json'
+DEFAULT_CONFIG = {
+    "gen-reader-with": "Python",
+    "src-path": "./"
+}
+
+
+def read_config():
+    logger = logging.getLogger("Configurator")
+    try:
+        fp = open(CONFIGURATION)
+        configuration = json.load(fp)
+        fp.close()
+        if set(configuration.keys()) != set(DEFAULT_CONFIG.keys()):
+            logger.error("Invalid configuration")
+            logger.debug("DEBUG - NO FALL BACK")
+            # return None
+    except IOError as ioe:
+        logger.error("Cannot access configuration: IOError: %s", CONFIGURATION)
+    except ValueError as ve:
+        logger.error("Cannot parse configuration: ValueError: %s",
+                     CONFIGURATION)
+    except Exception as e:
+        logger.error("Undefined error: %s", e)
+    else:
+        return configuration
+
+
+def main():
+    logger = logging.getLogger("Main")
+    configure = read_config()
+    if configure is None:
+        logger.warning("Config failed. Falling back to default.")
+        configure = DEFAULT_CONFIG
+    # build reader
+    logger.info("BUILDING READER")
+    logsys = configure['logsys']
+    pattern = configure['pattern']
+    make_reader(logsys, pattern)
+
+
+if __name__ == '__main__':
+    main()
